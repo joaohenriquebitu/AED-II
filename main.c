cc14c31b7a29b6d026b77f907473c120f500de4d
joaohenriquebitu <134982253+joaohenriquebitu@users.noreply.github.com>
2024-11-15 17:01:08 -0300

    criado struct paciente

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 
 
 int main(){
-  
-printf("teste");
+
+
+
+  struct Paciente {
+    float peso;
+    float altura;
+    char sintomas[256];
+    char medicacoes[256];
+    int idade;
+    char telefone[15];
+    int id;
+    int prioridade;
+  };
+
 }
